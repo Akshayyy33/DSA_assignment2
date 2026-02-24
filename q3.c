095deef755dc4090633eb73ccd5e85c4442fdada
Akshay Shukla <akshay14yrs@gmail.com>
2026-02-24 23:48:03 +0530

    Update q3.c

--- q3.c
+++ q3.c
@@ -1,55 +1,85 @@
 /* Q3: Perform insertion at beginning, end and after a given node */
 #include <stdio.h>
 #include <stdlib.h>
-struct NodeRec {
-    int info;
-    struct NodeRec* nextLink;
-};
-void insertFront(struct NodeRec** startNode, int newVal) {
-    struct NodeRec* freshNode = (struct NodeRec*)malloc(sizeof(struct NodeRec));
-    freshNode->info = newVal;
-    freshNode->nextLink = *startNode;
-    *startNode = freshNode;
-}
-void insertRear(struct NodeRec** startNode, int newVal) {
-    struct NodeRec* freshNode = (struct NodeRec*)malloc(sizeof(struct NodeRec));
-    freshNode->info = newVal;
-    freshNode->nextLink = NULL;
 
-    if(*startNode == NULL) {
-        *startNode = freshNode;
+struct InsNode {
+    int dataVal;
+    struct InsNode* nextPtr;
+};
+void insertBeginning(struct InsNode** headRef, int val) {
+    struct InsNode* newNode = malloc(sizeof(struct InsNode));
+    newNode->dataVal = val;
+    newNode->nextPtr = *headRef;
+    *headRef = newNode;
+}
+void insertEnd(struct InsNode** headRef, int val) {
+    struct InsNode* newNode = malloc(sizeof(struct InsNode));
+    newNode->dataVal = val;
+    newNode->nextPtr = NULL;
+
+    if(*headRef == NULL) {
+        *headRef = newNode;
         return;
     }
-    struct NodeRec* walkNode = *startNode;
-    while(walkNode->nextLink != NULL)
-        walkNode = walkNode->nextLink;
-    walkNode->nextLink = freshNode;
-}
-void insertAfterKey(struct NodeRec* startNode, int keyVal, int newVal) {
-    while(startNode != NULL && startNode->info != keyVal)
-        startNode = startNode->nextLink;
+    struct InsNode* walk = *headRef;
+    while(walk->nextPtr != NULL)
+        walk = walk->nextPtr;
 
-    if(startNode != NULL) {
-        struct NodeRec* freshNode = (struct NodeRec*)malloc(sizeof(struct NodeRec));
-        freshNode->info = newVal;
-        freshNode->nextLink = startNode->nextLink;
-        startNode->nextLink = freshNode;
-    }
-    else
-        printf("Key not found\n");
+    walk->nextPtr = newNode;
 }
-void showList(struct NodeRec* startNode) {
-    while(startNode != NULL) {
-        printf("%d -> ", startNode->info);
-        startNode = startNode->nextLink;
+void insertAfter(struct InsNode* headRef, int keyVal, int newVal) {
+    while(headRef != NULL && headRef->dataVal != keyVal)
+        headRef = headRef->nextPtr;
+
+    if(headRef == NULL) {
+        printf("Key not found!\n");
+        return;
+    }
+    struct InsNode* newNode = malloc(sizeof(struct InsNode));
+    newNode->dataVal = newVal;
+    newNode->nextPtr = headRef->nextPtr;
+    headRef->nextPtr = newNode;
+}
+void displayList(struct InsNode* headRef) {
+    while(headRef != NULL) {
+        printf("%d -> ", headRef->dataVal);
+        headRef = headRef->nextPtr;
     }
     printf("NULL\n");
 }
 int main() {
-    struct NodeRec* startNode = NULL;
-    insertFront(&startNode, 5);
-    insertRear(&startNode, 20);
-    insertAfterKey(startNode, 5, 10);
-    showList(startNode);
+    struct InsNode* headRef = NULL;
+    int choice, value, key;
+    do {
+        printf("\n1.Insert Beginning\n2.Insert End\n3.Insert After\n4.Display\n5.Exit\n");
+        printf("Enter choice: ");
+        scanf("%d", &choice);
+
+        switch(choice) {
+            case 1:
+                printf("Enter value: ");
+                scanf("%d", &value);
+                insertBeginning(&headRef, value);
+                break;
+
+            case 2:
+                printf("Enter value: ");
+                scanf("%d", &value);
+                insertEnd(&headRef, value);
+                break;
+
+            case 3:
+                printf("Enter key value: ");
+                scanf("%d", &key);
+                printf("Enter new value: ");
+                scanf("%d", &value);
+                insertAfter(headRef, key, value);
+                break;
+
+            case 4:
+                displayList(headRef);
+                break;
+        }
+    } while(choice != 5);
     return 0;
 }
